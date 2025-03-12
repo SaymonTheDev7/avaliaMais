1f1c318d8eae9c477689fcf85ffddb000015972c
Saymon Oliveira de Castro <saymon_castro@estudante.sesisenai.org.br>
2025-03-12 16:51:15 -0300

    Prime commit.

---
+++ backend/src/main/java/net/weg/avaliaMais/model/User.java
@@ -0,0 +1,6 @@
+package net.weg.avaliaMais.model;
+
+public class User {
+
+    private String clov
+}
---
+++ backend/src/test/java/net/weg/avaliaMais/AvaliaMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package net.weg.avaliaMais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
