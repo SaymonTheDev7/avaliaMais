747265ebbc1667d2adbc71168cfb0f8e2d763af2
André Felipe Witt <andreflipewitt@gmail.com>
2025-03-31 20:58:02 -0300

    Alteração no titulo do dados turma
    Alteração para o padrão dos titulos

--- frontend/src/app/pedagogical-technique/dados-turma-pedagogical-technique/page.tsx
+++ frontend/src/app/pedagogical-technique/dados-turma-pedagogical-technique/page.tsx
@@ -9,15 +9,14 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-muted">
       <Header />
-      <div className="flex items-center space-x-2 p-4 text-[#02335E]">
-        <button onClick={() => {}} className="text-2xl mr-4">
-          <ChevronLeft size={32} strokeWidth={2.5} />
-        </button>
-        <div className="flex flex-col items-start">
-          <span className="text-3xl font-bold">TURMA AI PSIN 2023/2</span>
-          <div className="border-b-4 border-[#02335E] w-full max-w-[400px]"></div>
+      <div className="flex items-center mb-6 px-4">
+          <a href="#" className="text-[#003366] mr-4">
+            <ChevronLeft size={28} strokeWidth={2.5} />
+          </a>
+          <h1 className="text-2xl sm:text-3xl font-bold text-[#003366] uppercase border-b-2 border-[#003366] pb-1">
+          TURMA AI PSIN 2023/2
+          </h1>
         </div>
-      </div>
       <main className="flex justify-center items-center p-4">
         <ClassForm />
       </main>
